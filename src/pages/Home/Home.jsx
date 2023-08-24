c5df61f3737e7a6a3b2b1e3d4e34db30f894d379
Sadakat Hossain <sadakat.hossain11@gmail.com>
2023-08-24 07:02:46 +0600

    Setup DaisiUI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h2 className="text-4xl">This is home page</h2>
+            <button className="btn btn-ghost">Ghost</button>
         </div>
     );
 };
